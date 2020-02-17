5744ecf2834ff3b69376807bc60eab6ad3121e78
Evert Maas <r0370888@student.thomasmore.be>
2020-02-17 21:38:45 +0100

    g fucking g

---
+++ AzureASP/AzureASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
